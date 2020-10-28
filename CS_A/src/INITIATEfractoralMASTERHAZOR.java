ff03b13a7f884c6bd23b1bdd2e43c0b84a9c388e
Moriz82 <william.dedominico88@gmail.com>
2020-10-28 09:36:05 -0400

    oo

---
+++ CS_A/src/INITIATEfractoralMASTERHAZOR.java
@@ -0,0 +1,12 @@
+public class INITIATEfractoralMASTERHAZOR {
+    public INITIATEfractoralMASTERHAZOR(int angorthummm) {
+        while (true) {
+            bigFwactwoldraw(angorthummm);
+        }
+    }
+
+
+    public static void bigFwactwoldraw(int a) {
+
+    }
+}
--- UIL/src/Lost_v2.java
+++ UIL/src/Lost_v2.java
@@ -5,22 +5,28 @@ public class Lost_v2 {
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(new File("student/lost.dat"));
         int amntMazes = scan.nextInt();
-
         for (int i = 0; i < amntMazes; i++) {
-            System.out.println(i);
+            int amntSpaces = 2;
             int rows = scan.nextInt();
             int columns = scan.nextInt();
             //throw the /n because it causes errors
             scan.nextLine();
             char[][] mazeArr = new char[rows][rows];
-            scan.nextLine();
-            for (int j = 0; j < rows; j++) {
+            for (int j = 0; j < rows; j+=2) {
+                System.out.println(rows);
+                boolean found = false;
                 String row1 = scan.nextLine();
                 String row2 = scan.nextLine();
                 for (int k = 0; k < columns; k++) {
                     mazeArr[0][j] = row1.charAt(j);
                     mazeArr[1][j] = row2.charAt(j);
-                }//jake not big gay
+                }
+                for (int k = 0; k < columns; k++) {
+                    if (mazeArr[0][k] == mazeArr[1][k] && !found){
+                        amntSpaces += k+1;
+                        found = true;
+                    }
+                }
             }
         }
     }
